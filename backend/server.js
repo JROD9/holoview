3ef4419c95e670c068796dc97b07ec4122c7445e
Jonathan <jonathan.s9522@gmail.com>
2025-02-02 05:26:57 -0500

    commit before new branch

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 const app = express();
 const port = 8080;
 dotenv.config();
 
-mongoose.connect(process.env.DB_URI, )
+mongoose.connect(process.env.DB_URI)
     .then(() => {
         console.log('db connected successfully');
-        return app.listen(port);
+        app.listen(port);
     })
     .then(() => console.log('server running'))
     .catch((err) => console.log("err: ", err.message))
