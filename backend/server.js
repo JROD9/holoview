bb13870d6a80c9df9c9ba4402c4c9d832f31dee7
Jonathan <jonathan.s9522@gmail.com>
2025-02-01 19:38:09 -0500

    commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+giimport express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+const app = express();
+const port = 8080;
+dotenv.config();
+
+mongoose.connect(process.env.DB_URI, )
+    .then(() => {
+        console.log('db connected successfully');
+        return app.listen(port);
+    })
+    .then(() => console.log('server running'))
+    .catch((err) => console.log("err: ", err.message))
+
+
